7d349d8c6b568c95459ea97def8eb65cc44792e0
ILDARON123 <zaitkulov4@gmail.com>
2023-12-07 14:46:14 +0500

    commit1

---
+++ index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Youngster</title>
+</head>
+
+<body>
+    <div class="all">
+        <div class="lefter">
+            <div class="lefter_main">
+                <div class="logo">
+                    <img src="/img/LOGO.png" alt="logo">
+                </div>
+                <nav>
+                    <a href="/">
+                        <div class="page"><img src="/img/page_home.png" alt="">Главная</div>
+                    </a>
+                    <a href="#">
+                        <div class="page"><img src="/img/page_interesting.png" alt="">Интересное</div>
+                    </a>
+                    <a href="#">
+                        <div class="page"><img src="/img/page_notifications.png" alt="">Уведомления</div>
+                    </a>
+                    <a href="#">
+                        <div class="page"><img src="/img/page_create.png" alt="">Создать</div>
+                    </a>
+                    <a href="#">
+                        <div class="page_profile"><img src="./img/your_profile.jpg" alt="">Профиль</div>
+                    </a>
+                </nav>
+            </div>
+
+            <div class="more">
+                <a href="#">
+                    <div class="page"><img src="/img/page_settings.png" alt="">Настройки</div>
+                </a>
+            </div>
+        </div>
+
+
+        <div class="main">
+            <div class="stories">
+                <div class="story">
+                    <img src="./img/story_profile.jpg" width="55px" height="55px" alt="">
+                    <h3>Имя профиляпрофиля</h3>
+                </div>
+                <div class="story">
+                    <img src="./img/story_profile.jpg" width="55px" height="55px" alt="">
+                    <h3>Имя профиля</h3>
+                </div>
+                <div class="story">
+                    <img src="./img/story_profile.jpg" width="55px" height="55px" alt="">
+                    <h3>Имя профиля</h3>
+                </div>
+                <div class="story">
+                    <img src="./img/story_profile.jpg" width="55px" height="55px" alt="">
+                    <h3>Имя профиля</h3>
+                </div>
+                <div class="story">
+                    <img src="./img/story_profile.jpg" width="55px" height="55px" alt="">
+                    <h3>Имя профиля</h3>
+                </div>
+                <div class="story">
+                    <img src="./img/story_profile.jpg" width="55px" height="55px" alt="">
+                    <h3>Имя профиля</h3>
+                </div>
+                <div class="story">
+                    <img src="./img/story_profile.jpg" width="55px" height="55px" alt="">
+                    <h3>Имя профиля</h3>
+                </div>
+                <div class="story">
+                    <img src="./img/story_profile.jpg" width="55px" height="55px" alt="">
+                    <h3>Имя профиля</h3>
+                </div>
+            </div>
+
+            <div class="info">
+                <div class="info_about">
+                    <img src="./img/your_profile.jpg" width="45px" height="45px" alt="">
+                    <div class="info_about_h3">
+                        <h2>Имя профиля</h2>
+                        <h3>Имя Фамилия</h3>
+                    </div>
+                </div>
+                <div class="info_recommend">
+                    <h2>Рекомендации для вас</h2>
+                    <div class="info_recomend_about">
+                        <img src="./img/your_profile.jpg" width="45px" height="45px" alt="">
+                        <div class="info_recomend_h3">
+                            <h1>Имя профиля</h1>
+                            <h3>Имя Фамилия</h3>
+                        </div>
+                        <a href="#">Подписаться</a>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="news">
+                <div class="news_about">
+                    <img src="./img/your_profile.jpg" width="35px" height="35px" alt="">
+                    <h3>Имя профиля</h3>
+                </div>
+                <div class="news_post">
+                    <img src="./img/your_profile.jpg" alt="">
+                    <div class="news_post_reactions">
+                        <img src="./img/page_notifications.png" width="25px" alt="">
+                        <img src="./img/icon_coment.png" width="25px" alt="">
+                        <img src="./img/icon_share.png" width="25px" alt="">
+                    </div>
+                    <h3>123 отметок "Нравится"</h3>
+                    <div class="news_post_info">
+                        <h2>Имя профиля</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                    </div>
+                </div>
+
+                <div class="news_about">
+                    <img src="./img/your_profile.jpg" width="35px" height="35px" alt="">
+                    <h3>Имя профиля</h3>
+                </div>
+                <div class="news_post">
+                    <img src="./img/post4.jpg" alt="">
+                    <div class="news_post_reactions">
+                        <img src="./img/page_notifications.png" width="25px" alt="">
+                        <img src="./img/icon_coment.png" width="25px" alt="">
+                        <img src="./img/icon_share.png" width="25px" alt="">
+                    </div>
+                    <h3>123 отметок "Нравится"</h3>
+                    <div class="news_post_info">
+                        <h2>Имя профиля</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+
+
+
+</body>
+
+</html>
